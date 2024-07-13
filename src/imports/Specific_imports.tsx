5de042087339e8dca5a9cf31ec081ae6a6f3c35f
Mohit Saini <mailtomohit2002@gmail.com>
2024-07-13 14:03:37 +0530

    feat : Added winston custom logger

--- src/imports/Specific_imports.tsx
+++ src/imports/Specific_imports.tsx
@@ -4,3 +4,4 @@ export { default as Orientation } from 'filepond-plugin-image-exif-orientation';
 export { default as Preview } from 'filepond-plugin-image-preview';
 
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+
