13d435a42b0f7ef97236006da05a46b5dba2d8e4
Mohit Saini <mailtomohit2002@gmail.com>
2024-07-13 17:29:19 +0530

    feat : Added logs to api routes

--- src/app/api/youtube/livestream/route.ts
+++ src/app/api/youtube/livestream/route.ts
@@ -2,8 +2,12 @@ import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import { GetLivestreams } from '@/app/api/services/livestreams';
+import { createLoggerWithLabel } from '@/app/api/utils/logger'
+
+const logger = createLoggerWithLabel('LiveStream')
 
 export const GET = async () => {
+  logger.info('Fetching user livestreams info...')
   const session = await getServerSession(authOptions);
   // @ts-ignore
   const userId = session?.user.id;
@@ -16,6 +20,7 @@ export const GET = async () => {
     const livestreams = await GetLivestreams(userId);
     return NextResponse.json({ data: livestreams }, { status: 200 });
   } catch (error) {
+    logger.error(`Error fetching user livestreams info : ${error}`)
     return NextResponse.json(
       { message: 'Error while fetching user livestreams', error },
       { status: 401 }
