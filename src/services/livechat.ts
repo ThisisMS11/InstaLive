9c37d0a1f622897584eefe2b172885798d009b53
Mohit Saini <mailtomohit2002@gmail.com>
2024-08-11 18:43:44 +0530

    fix : Something

--- src/services/livechat.ts
+++ src/services/livechat.ts
@@ -1,11 +1,13 @@
 import useSWR from 'swr';
 import AxiosFetcher from '@/utils/axiosFetcher';
 import { LiveChatMessage } from '@/app/types/livechat'
-
 export const useGetLiveMessages = (livechatId: string) => {
+
     console.info('Fetching All Livechat Messages ...');
+
+    const livechatId2  = 'Cg0KC1pJSzNOMkpsMkNJKicKGFVDZWYxLThlT3BKZ3VkN3N6VlBsWlFBURILWklLM04ySmwyQ0k'
     const { data, error, isLoading } = useSWR(
-        `/api/youtube/livechat/Cg0KC2ZQSWY5bXJsdkZnKicKGFVDZWYxLThlT3BKZ3VkN3N6VlBsWlFBURILZlBJZjltcmx2Rmc`,
+        `/api/youtube/livechat/${livechatId2}`,
         AxiosFetcher,
         {refreshInterval : 500}
     );
