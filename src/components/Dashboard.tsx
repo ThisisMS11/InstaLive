6595d80832c6597676d8addcd48558b223366b38
Mohit Saini <mailtomohit2002@gmail.com>
2024-10-17 22:50:19 +0530

    fix : some fixes before  pushing

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -22,7 +22,7 @@ import {
 import { ThumbsUp, Eye, MessageSquareText } from 'lucide-react';
 import BroadCastCell from './BroadCastCell';
 import { useAppSelector } from '@/imports/Redux_imports';
-
+import logo from '@/app/assets/logo.png';
 function Dashboard({
   buttonRef,
   broadcasts,
@@ -50,7 +50,7 @@ function Dashboard({
               className="flex items-center gap-2 font-semibold"
               prefetch={false}
             >
-              <VideoIcon className="h-6 w-6" />
+              <Image src={logo} alt="Logo" className="h-6 w-6" />
               <span className="">InstaLive</span>
             </Link>
           </div>
@@ -412,27 +412,6 @@ function UsersIcon(props) {
       <circle cx="9" cy="7" r="4" />
       <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
       <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-    </svg>
-  );
-}
-
-// @ts-ignore
-function VideoIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m16 13 5.223 3.482a.5.5 0 0 0 .777-.416V7.87a.5.5 0 0 0-.752-.432L16 10.5" />
-      <rect x="2" y="6" width="14" height="12" rx="2" />
     </svg>
   );
 }
